125b446f2df40ad07679477d2ac6e46415c0cc0f
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2024-12-24 15:20:15 -0300

    wip

--- execute_chatester.sh
+++ execute_chatester.sh
@@ -15,11 +15,14 @@ execute_command() {
     echo "Problem in execution of $folder/$env_file: $output" >>result.log
   fi
 }
-
-
+export TF_ENABLE_ONEDNN_OPTS=0
+export CUDA_VISIBLE_DEVICES=""
 # Run main.py in the background
 python main.py &
 flask_pid=$!
+# Wait for Flask to initialize (use sleep or health check)
+echo "Waiting for Flask app to initialize..."
+sleep 15  # Adjust this as needed
 # Loop through each folder in the "envs" directory
 for folder in enfiles/*; do
   if [ -d "$folder" ]; then
--- main.py
+++ main.py
@@ -54,14 +54,12 @@ def generate_text():
     max_tokens = data.get('max_tokens', 512)
     print("maxtokens:"+str(max_tokens))
     temperature = data.get('temperature', 0.7)
-    prompt,sysmessage,usermessage = generate_prompt(messages=messages,model_name=model_name)
+    prompt,sysmessage,usermessage = auxfunctions.generate_prompt(messages=messages,model_name=model_name)
     if not model_name or not prompt:
         return jsonify({"error": "'model' and 'prompt' are required."}), 400
-    try:
-        output = auxfunctions.generate_model(prompt=prompt,model_name=model_name,
+
+    output = auxfunctions.generate_model(prompt=prompt,model_name=model_name,
             temperature=temperature,max_tokens=max_tokens)
-    except Exception as e:
-        jsonify({"error": str(e)}), 400
     #print("rawresponse:" + output)
     # Split the output from the superprompt length
     if(model_name in ["starcoder2-3b-Q8_0.gguf"]):
@@ -123,6 +121,7 @@ def openai_to_gemini():
         temperature = data.get('temperature', 0.7)
         sysmessage = ""
         usermessage = ""
+        messagesNew = []
         for messageOb in messages:
             messagesNew.append({"role": "user", "parts": [{"text": messageOb['content']}]})
 
