7a3d37a86abcd35d451222ee9c19b9bc83b44afc
Esdras Caleb <esdrascaleb@gmail.com>
2024-12-24 10:57:08 -0300

    tests

--- testmodel.py
+++ testmodel.py
@@ -5,7 +5,7 @@ from loadenv import load_env_file
 env_data = load_env_file(file_path)
 # Path to the folder containing GGUF model files
 models_folder = os.path.join(env_data.get("model_dir","./models"),"gguf")
-models=["HuggingFaceTB/SmolLM2-1.7B-Instruct"]
+models=["internlm2_5-7b-chat-q8_0.gguf"]
 prompt="give me e small java class that calculate the area of a polygon"
 messages=[
     {
@@ -22,21 +22,19 @@ messages=[
 for model_name in models:
     print(f"Testing model: {model_name}")
 
-    try:
-        prompt,sysmessage,usermessage = generate_prompt(messages=messages,model_name=model_name)
+    
+    prompt,sysmessage,usermessage = generate_prompt(messages=messages,model_name=model_name)
 
-        # Create chat completion
-        result = generate_model(
-            prompt=prompt,
-            model_name=model_name,
-            max_tokens=256,
-            temperature=0.7
-        )
+    # Create chat completion
+    result = generate_model(
+        prompt=prompt,
+        model_name=model_name,
+        max_tokens=256,
+        temperature=0.7
+    )
 
-        # Print the output
-        print(f"Response from {model_name}:")
-        print(result)
-        clear_models()
-    except Exception as e:
-        print(f"An error occurred while testing {model_name}: {e}")
-        print(e)
+    # Print the output
+    print(f"Response from {model_name}:")
+    print(result)
+    clear_models()
+    
