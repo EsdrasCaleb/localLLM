c25c4e967f941e09fac1fb16d7a2b13ca05d9a35
Esdras Caleb <esdrascaleb@gmail.com>
2024-12-23 23:00:04 -0300

    wip

--- flaskbatch.sh
+++ flaskbatch.sh
@@ -10,6 +10,7 @@ module load cuda/11.7               # CUDA version (if using GPUs)
 
 source $HOME/.bashrc
 # Activate virtual environment (if needed)
+conda activate llm_env
 source $HOME/localLLM/vllm_env/bin/activate  # Update with your virtual environment path
 
 # Function to execute a command and capture its output
