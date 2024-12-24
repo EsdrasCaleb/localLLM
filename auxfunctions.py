11de2ea9f5e292a3bae7795d97c2d1d701a38f5a
Esdras Caleb <esdrascaleb@gmail.com>
2024-12-24 12:37:48 -0300

    erase cache

--- auxfunctions.py
+++ auxfunctions.py
@@ -236,13 +236,13 @@ def download_model(model_name,file=None):
             snapshot_download(repo_id=model_name, local_dir=os.path.join(MODEL_DIR, 'gguf'), token=HF_TOKEN, 
                 allow_patterns=[file],cache_dir=local_cache)
             if(os.path.exists(local_cache)):
-                shutil.rmtree(cache_dir)
+                shutil.rmtree(local_cache)
         else:
             local_cache = os.path.join(model_path,".cache")
             snapshot_download(repo_id=model_name, local_dir=model_path,cache_dir=local_cache,
             token=HF_TOKEN,ignore_patterns=["*onnx*","runs","*guff*"])
             if(os.path.exists(local_cache)):
-                shutil.rmtree(cache_dir)
+                shutil.rmtree(local_cache)
 
         return f"Model '{model_name}' downloaded successfully."
        
