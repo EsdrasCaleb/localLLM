bcd92a9853361349f92af7c574c909b8fc532561
Esdras Caleb <esdrascaleb@gmail.com>
2024-12-24 11:18:52 -0300

    fix patters

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def download_model(model_name,file=None):
     try:
         if(file):
             snapshot_download(repo_id=model_name, local_dir=os.path.join(MODEL_DIR, 'gguf'), token=HF_TOKEN, 
-                allow_patterns=[file.replace(".",r"\.")])
+                allow_patterns=[file])
         else:
             snapshot_download(repo_id=model_name, local_dir=model_path, token=HF_TOKEN,ignore_patterns=["*onnx*","runs","*guff*"])
         return f"Model '{model_name}' downloaded successfully."
