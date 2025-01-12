860b397f140e453ec8c61b9558d99d71444ae66e
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2025-01-12 16:47:59 -0300

    test

--- flaskbatchenv_gpu.sh
+++ flaskbatchenv_gpu.sh
@@ -11,8 +11,10 @@ module load cmake
 source $HOME/.bashrc
 # Activate virtual environment (if needed)
 conda activate llm_env_gpu
-conda install gcc_linux-64 libstdcxx-ng cmake ninja llama-cpp-python
+conda install gcc_linux-64 libstdcxx-ng cmake ninja
+conda install -c conda-forge cmake make gcc libgcc gxx -y
 pip install --upgrade -r requirements.txt
+pip install --no-cache-dir llama-cpp-python
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # Function to execute a command and capture its output
 execute_command() {
