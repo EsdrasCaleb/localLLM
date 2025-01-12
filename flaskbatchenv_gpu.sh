0af7f4359db8d3aab306f8ba68b338f31f4c7981
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2025-01-12 15:56:19 -0300

    test

--- flaskbatchenv_gpu.sh
+++ flaskbatchenv_gpu.sh
@@ -7,6 +7,7 @@
 # Load modules (adjust based on your environment)
 #module load python/3.10              # Python version
 module load libraries/cuda/12.6              # CUDA version (if using GPUs)
+module load cmake
 export CXX=$CONDA_PREFIX/bin/x86_64-redhat-linux-c++
 source $HOME/.bashrc
 # Activate virtual environment (if needed)
