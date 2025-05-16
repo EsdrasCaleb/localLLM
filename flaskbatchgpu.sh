b6cadbb1566c88c57a6dc2bdeb0a2e808f117b1e
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2025-05-16 18:03:52 -0300

    qos

--- flaskbatchgpu.sh
+++ flaskbatchgpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=flask_uni        # Job name
 #SBATCH --output=flask_uni_%j.log    # Log file (%j = job ID)
-#SBATCH --time=20:00:00            # 2-00:00:00
+#SBATCH --time=2-00:00:00            # ate 2 dias
 #SBATCH --gres=gpu:1                 # Request 1 GPU
 #SBATCH --qos=preempt
 
