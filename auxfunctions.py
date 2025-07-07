2d17ebbee918466766bc1d9f5f29a639b78e3ded
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2025-07-07 15:30:18 -0300

    wip

--- auxfunctions.py
+++ auxfunctions.py
@@ -85,8 +85,7 @@ def generate_prompt(messages,model_name):
             prompt = usermessage
     elif(model_name in ["Qwen/Qwen2.5-Coder-0.5B-Instruct","Qwen/Qwen2.5-Coder-1.5B-Instruct","Qwen/Qwen2.5-Coder-7B-Instruct","infly/OpenCoder-1.5B-Instruct",
     "HuggingFaceTB/SmolLM2-1.7B-Instruct","Salesforce/xLAM-1b-fc-r","ibm-granite/granite-3.1-1b-a400m-instruct",
-    "deepseek-ai/deepseek-coder-1.3b-instruct","deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B","deepseek-ai/deepseek-coder-6.7b-instruc","tiiuae/Falcon3-1B-Instruct",
-                        "google/gemma-2-2b-it"] or
+    "deepseek-ai/deepseek-coder-1.3b-instruct","deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B","deepseek-ai/deepseek-coder-6.7b-instruc","tiiuae/Falcon3-1B-Instruct"] or
     model_name.endswith(".gguf")):
         prompt = messages
     else:
